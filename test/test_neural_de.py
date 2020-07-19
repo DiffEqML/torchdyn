f792efccc824c0e55dd8b75589fd1a55e81d669e
Zymrael <mic.poli@hotmail.it>
2020-07-19 19:46:12 +0900

    [tests][bug] improve coverage, fix energy models

--- test/test_neural_de.py
+++ test/test_neural_de.py
@@ -138,3 +138,28 @@ def test_vanilla_galerkin():
     learn = TestLearner(model, trainloader=trainloader)
     trainer = pl.Trainer(min_epochs=10, max_epochs=30)
     trainer.fit(learn) 
+    
+def test_2nd_order():
+    """2nd order (MLP) Galerkin Neural ODE"""
+    d = ToyDataset()
+    X, yn = d.generate(n_samples=512, dataset_type='spirals', noise=.4)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    X_train = torch.Tensor(X).to(device)
+    y_train = torch.LongTensor(yn.long()).to(device)
+    train = data.TensorDataset(X_train, y_train)
+
+    trainloader = data.DataLoader(train, batch_size=len(X), shuffle=False) 
+
+    f = nn.Sequential(DepthCat(1),
+                      nn.Linear(5, 64),
+                      nn.Tanh(), 
+                      DepthCat(1),
+                      nn.Linear(65, 2))
+
+    model = nn.Sequential(Augmenter(augment_idx=1, augment_func=nn.Linear(2, 2)),
+                          NeuralDE(f, solver='dopri5', order=2)
+                         ).to(device)
+    learn = TestLearner(model, trainloader=trainloader)
+    trainer = pl.Trainer(min_epochs=10, max_epochs=30)
+    trainer.fit(learn) 
+
