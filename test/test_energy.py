8e7ac1b35f0218957dd2483a07ca4dbceb1798d4
Zymrael <mic.poli@hotmail.it>
2021-06-15 17:01:43 +0900

    update requirements and remove device from energy tests

--- test/test_energy.py
+++ test/test_energy.py
@@ -11,16 +11,15 @@ def test_stable_neural_de(testlearner):
     """Stable: basic functionality"""
     d = ToyDataset()
     X, yn = d.generate(n_samples=512, dataset_type='moons', noise=.4)
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    X_train = torch.Tensor(X).to(device)
-    y_train = torch.LongTensor(yn.long()).to(device)
+    X_train = torch.Tensor(X)
+    y_train = torch.LongTensor(yn.long())
     train = data.TensorDataset(X_train, y_train)
     trainloader = data.DataLoader(train, batch_size=len(X), shuffle=False)
     f = GNF(nn.Sequential(
             nn.Linear(2, 64),
             nn.Tanh(),
             nn.Linear(64, 1)))
-    model = NeuralODE(f).to(device)
+    model = NeuralODE(f)
     learn = testlearner(model, trainloader=trainloader)
     trainer = pl.Trainer(min_epochs=10, max_epochs=30)
     trainer.fit(learn)
@@ -29,16 +28,15 @@ def test_hnn(testlearner):
     """HNN: basic functionality"""
     d = ToyDataset()
     X, yn = d.generate(n_samples=32, dataset_type='moons', noise=.4)
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    X_train = torch.Tensor(X).to(device)
-    y_train = torch.LongTensor(yn.long()).to(device)
+    X_train = torch.Tensor(X)
+    y_train = torch.LongTensor(yn.long())
     train = data.TensorDataset(X_train, y_train)
     trainloader = data.DataLoader(train, batch_size=len(X), shuffle=False)
     f = HNN(nn.Sequential(
             nn.Linear(2, 64),
             nn.Tanh(),
             nn.Linear(64, 1)))
-    model = NeuralODE(f).to(device)
+    model = NeuralODE(f)
     learn = testlearner(model, trainloader=trainloader)
     trainer = pl.Trainer(min_epochs=10, max_epochs=30)
     trainer.fit(learn)
@@ -47,16 +45,15 @@ def test_lnn(testlearner):
     """LNN: basic functionality"""
     d = ToyDataset()
     X, yn = d.generate(n_samples=32, dataset_type='moons', noise=.4)
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    X_train = torch.Tensor(X).to(device)
-    y_train = torch.LongTensor(yn.long()).to(device)
+    X_train = torch.Tensor(X)
+    y_train = torch.LongTensor(yn.long())
     train = data.TensorDataset(X_train, y_train)
     trainloader = data.DataLoader(train, batch_size=len(X), shuffle=False)
     f = LNN(nn.Sequential(
             nn.Linear(2, 64),
             nn.Tanh(),
             nn.Linear(64, 1)))
-    model = NeuralODE(f).to(device)
+    model = NeuralODE(f)
     learn = testlearner(model, trainloader=trainloader)
     trainer = pl.Trainer(min_epochs=10, max_epochs=30)
     trainer.fit(learn)
