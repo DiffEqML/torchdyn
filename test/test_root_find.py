c674f2821fbfb4a55f3765c3354f81424834ae77
massastrello <massa95>
2021-06-24 22:12:46 +0900

    fixed root_find test

--- test/test_root_find.py
+++ test/test_root_find.py
@@ -6,9 +6,12 @@ from .conftest import *
 @pytest.mark.parametrize('func', [quad, cubic, ackley])
 @pytest.mark.parametrize('method', ['broyden'])
 def test_root_iteration(method, func):
-    z0 = 0.1 * torch.randn(4, 2)
+    z0 = 1 * torch.randn(4, 2)
+    if func == rosenbrock:
+        z0 = torch.Tensor([[-1., 1.]])
     root, log = root_find(func, z0,
                          alpha=1, search_method='armijo',
                          f_tol=1e-3, f_rtol=1e-3, x_tol=1e-2, x_rtol=1e-2,
                          maxiters=1000, method=method)
+    print(func(root))
     assert (func(root) <= 1e-2).all()
