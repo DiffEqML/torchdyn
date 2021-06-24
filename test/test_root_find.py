2ffdb89e57fc63e8fbf7df367bffa8244127d489
massastrello <massa95>
2021-06-24 21:52:32 +0900

    fixed root_find test

--- test/test_root_find.py
+++ test/test_root_find.py
@@ -7,7 +7,6 @@ from .conftest import *
 @pytest.mark.parametrize('method', ['broyden'])
 def test_root_iteration(method, func):
     z0 = 0.1 * torch.randn(4, 2)
-
     root, log = root_find(func, z0,
                          alpha=1, search_method='armijo',
                          f_tol=1e-3, f_rtol=1e-3, x_tol=1e-2, x_rtol=1e-2,
