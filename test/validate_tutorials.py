5b558b267ecca572fbab649d4e19b975e2a0dd16
Zymrael <mic.poli@hotmail.it>
2021-07-09 14:21:37 +0900

    improved automated nbook valid

--- test/validate_tutorials.py
+++ test/validate_tutorials.py
@@ -1,13 +1,21 @@
 import papermill as pm
 
-# TODO: extend tutorial automated valid
-TORCHDYN_NOTEBOOKS = [
-    '00_quickstart.ipynb']
-    #,
-    #'module1-neuralde/01_neural_ode_cookbook.ipynb',
+# TODO: extend tutorial automated validation, dry run parameter for quick training
+TORCHDYN_NOTEBOOKS_PATHS = [
+    '00_quickstart.ipynb',
+    'module1-neuralde/01_neural_ode_cookbook.ipynb']
     #'module2-numerics/02_hypersolver_odeint.ipynb']
 
 
-for notebook in TORCHDYN_NOTEBOOKS:
-    path_to_notebook = f'tutorials/{notebook}'
-    pm.execute_notebook(path_to_notebook, path_to_notebook)
+for path in TORCHDYN_NOTEBOOKS_PATHS:
+    notebook_path = path.split('/')
+    print(notebook_path)
+    if len(notebook_path) == 1: 
+        notebook = notebook_path[0]
+        path_to_notebook = f'tutorials/{notebook}'
+    else: 
+        module, notebook = notebook_path
+        path_to_notebook = f'tutorials/{module}/{notebook}'
+    path_to_output = f'tutorials/local_nbrun_{notebook}'
+    parameters=dict(dry_run=True)
+    pm.execute_notebook(path_to_notebook, path_to_output, parameters=parameters)
