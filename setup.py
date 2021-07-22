9ae5c2a8334fe8aa4ffb3258124fdf955cebc8a6
Michael Poli <34561392+Zymrael@users.noreply.github.com>
2021-07-22 20:37:58 +0900

    testing 0.5.0 (#105)

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import setup
 
 setup(
     name="torchdyn",
-    version="0.4.3",
+    version="0.5.0",
     author="Michael Poli and Stefano Massaroli",
     description="PyTorch package for all things neural differential equations.",
     url="https://github.com/DiffEqML/torchdyn",
