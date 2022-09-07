e4450868dc77dfc109516aba6e0eff1a0769489a
archis <joglekara@users.noreply.github.com>
2022-09-07 15:12:37 -0700

    docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ extensions = [
 
 # Napoleon settings
 napoleon_google_docstring = True
-napoleon_numpy_docstring = False
+napoleon_numpy_docstring = True
 napoleon_include_init_with_doc = False
 napoleon_include_private_with_doc = True
 
