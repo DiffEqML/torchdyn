e283f50887e4a4e4b93fee1882fb371756b89426
archis <joglekara@users.noreply.github.com>
2022-09-14 13:49:35 -0700

    refactor

--- docs/source/neuralode.rst
+++ docs/source/neuralode.rst
@@ -8,3 +8,15 @@ numerical program
 
 .. autoclass:: torchdyn.core.neuralde.NeuralODE
    :members:
+
+
+At a high level, it is clear that calling `NeuralODE` integrates the parameterized Differential Equation over the
+specified time-span. (The time-axis can be any other dimension. In pure ML applications, it's often an application
+specific virtual dimension)
+
+At a middle level, `NeuralODE`, upon instantiation, wraps the provided `vector_field` i.e. the NN that parameterizes the
+differential equation in a class called `DEFunc`.
+
+At a lower level, a `NeuralODE` inherits from `ODEProblem`. The `ODEProblem` is the object that ties together the
+vector field (e.g. an NN), the solver (e.g. RK4), and the sensitivity algorithm (e.g. adjoint) into a single construct.
+The `ODEProblem` requires a `DEFunc`. For this reason, the vector field is wrapped in a `DEFunc`.
--- torchdyn/core/utils.py
+++ torchdyn/core/utils.py
@@ -16,21 +16,28 @@ import torch
 from torchdyn.core.defunc import DEFuncBase, DEFunc
 import torch.nn as nn
 
+
 def standardize_vf_call_signature(vector_field, order=1, defunc_wrap=False):
     "Ensures Callables or nn.Modules passed to `ODEProblems` and `NeuralODE` have consistent `__call__` signature (t, x)"
-    
-    if issubclass(type(vector_field), nn.Module):
-        if 't' not in getfullargspec(vector_field.forward).args:
-            print("Your vector field callable (nn.Module) should have both time `t` and state `x` as arguments, "
-                "we've wrapped it for you.")
-            vector_field = DEFuncBase(vector_field, has_time_arg=False)
-    else: 
-        # argspec for lambda functions needs to be done on the function itself
-        if 't' not in getfullargspec(vector_field).args:
-            print("Your vector field callable (lambda) should have both time `t` and state `x` as arguments, "
-                "we've wrapped it for you.")
-            vector_field = DEFuncBase(vector_field, has_time_arg=False)   
-        else: vector_field = DEFuncBase(vector_field, has_time_arg=True) 
-    if defunc_wrap: return DEFunc(vector_field, order)
-    else: return vector_field
 
+    if issubclass(type(vector_field), nn.Module):
+        if "t" not in getfullargspec(vector_field.forward).args:
+            print(
+                "Your vector field callable (nn.Module) should have both time `t` and state `x` as arguments, "
+                "we've wrapped it for you."
+            )
+            vector_field = DEFuncBase(vector_field, has_time_arg=False)
+    else:
+        # argspec for lambda functions needs to be done on the function itself
+        if "t" not in getfullargspec(vector_field).args:
+            print(
+                "Your vector field callable (lambda) should have both time `t` and state `x` as arguments, "
+                "we've wrapped it for you."
+            )
+            vector_field = DEFuncBase(vector_field, has_time_arg=False)
+        else:
+            vector_field = DEFuncBase(vector_field, has_time_arg=True)
+    if defunc_wrap:
+        return DEFunc(vector_field, order)
+    else:
+        return vector_field
